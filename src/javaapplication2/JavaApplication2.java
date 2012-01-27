d9079521cc4db5867096f93b7a0f78bee7afce3f
Roland Aoun <abc@donotemail.me>
2012-01-26 21:32:11 -0500

    Second test

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author ke791434
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Test");
+        // TODO code application logic here
+    }
+}
